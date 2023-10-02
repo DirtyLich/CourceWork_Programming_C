bffcb89c8c6d38cd7880d667598155172a1d5b52
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-10-02 17:12:06 +0700

    change

--- main.c
+++ main.c
@@ -1,143 +1,174 @@
-#include <stdio.h>  // Подключение библиотеки для работы с вводом/выводом
-#include <stdlib.h> // Подключение библиотеки для работы с функциями общего назначения
-#include <string.h> // Подключение библиотеки для работы со строками
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define WINDOW_SIZE 32 // Определение размера скользящего окна
-#define BUFFER_SIZE 16  // Определение размера буфера
+#define WINDOW_SIZE 32
+#define BUFFER_SIZE 16
 
 typedef struct
-{ // Определение структуры Token
+{
     int offset;
     int length;
     char next;
 } Token;
-Token token = {0}; // Инициализация структуры Token нулями
 
 void compress(FILE *input, FILE *output)
-{                             // Функция для сжатия файла
-    char window[WINDOW_SIZE]; // Объявление массива для скользящего окна
-    char buffer[BUFFER_SIZE]; // Объявление массива для буфера
-    Token token;              // Объявление структуры Token
+{
+    char window[WINDOW_SIZE];
+    char buffer[BUFFER_SIZE];
+    Token token;
 
-    memset(window, 0, sizeof(window)); // Заполнение массива window нулями
-    memset(buffer, 0, sizeof(buffer)); // Заполнение массива buffer нулями
+    memset(window, 0, sizeof(window));
+    memset(buffer, 0, sizeof(buffer));
 
-    while (fgets(buffer, BUFFER_SIZE, input) != NULL)
-    {                                      // Чтение файла построчно в буфер
-        int bufferLength = strlen(buffer); // Определение длины буфера
-        int i = 0;                         // Инициализация переменной i
+    int bufferLength = 0;
+
+    while ((bufferLength = fread(buffer, 1, BUFFER_SIZE, input)) > 0)
+    {
+        int i = 0;
 
         while (i < bufferLength)
-        {                            // Цикл по буферу
-            int j = i - WINDOW_SIZE; // Определение начала скользящего окна
+        {
+            int j = i - WINDOW_SIZE;
             if (j < 0)
-            {          // Если начало окна меньше нуля
-                j = 0; // То начало окна равно нулю
+            {
+                j = 0;
             }
 
-            int maxLength = 0; // Инициализация переменной maxLength
-            int maxOffset = 0; // Инициализация переменной maxOffset
+            int maxLength = 0;
+            int maxOffset = 0;
 
             while (j < i)
-            {                   // Цикл по скользящему окну
-                int length = 0; // Инициализация переменной length
-
-                char *match = memchr(buffer + i, buffer[j], BUFFER_SIZE - i);
-                    if (match != NULL)
-                        {
-                            length = match - (buffer + i);
-                        }
-                    else
-                        {
-                            length = BUFFER_SIZE - i;
-                        }
-
-                // while (buffer[i + length] == buffer[j + length] && length < BUFFER_SIZE)
-                // {             // Поиск повторяющихся фрагментов
-                //     length++; // Увеличение длины повторяющегося фрагмента
-                // }
-
-                if (length > maxLength)
-                {                       // Если длина повторяющегося фрагмента больше максимальной длины
-                    maxLength = length; // То максимальная длина равна длине повторяющегося фрагмента
-                    maxOffset = i - j;  // А смещение равно разности позиций текущего и предыдущего вхождения фрагмента
+            {
+                int length = 0;
+                int k = 0;
+                while (i + k < bufferLength && buffer[j + k] == buffer[i + k])
+                {
+                    length++;
+                    k++;
                 }
 
-                j++; // Увеличение счетчика j
+                if (length > maxLength)
+                {
+                    maxLength = length;
+                    maxOffset = i - j;
+                }
+
+                j++;
             }
 
-            token.offset = maxOffset;           // Запись смещения в структуру Token
-            token.length = maxLength;           // Запись длины в структуру Token
-            token.next = buffer[i + maxLength]; // Запись следующего символа в структуру Token
+            token.offset = maxOffset;
+            token.length = maxLength;
+            token.next = buffer[i + maxLength];
 
-            fwrite(&token, sizeof(Token), 1, output); // Запись структуры Token в выходной файл
+            fwrite(&token, sizeof(Token), 1, output);
 
-            i += maxLength + 1; // Увеличение счетчика i
+            i += maxLength + 1;
+        }
+        char literal = buffer[bufferLength - 1];
+        if (literal == 0)
+        {
+            fputc(0, output); // Используем 0 для обозначения литерала
+        }
+        else
+        {
+            fputc(literal, output); // Записываем литерал как есть
         }
     }
 }
 
 void decompress(FILE *input, FILE *output)
-{                             // Функция для распаковки файла
-    char window[WINDOW_SIZE]; // Объявление массива для скользящего окна
-    Token token;              // Объявление структуры Token
+{
+    char window[WINDOW_SIZE];
+    Token token;
 
-    memset(window, 0, sizeof(window)); // Заполнение массива window нулями
+    memset(window, 0, sizeof(window));
+
+    int isLiteral = 0; // Флаг для определения литерала
 
     while (fread(&token, sizeof(Token), 1, input) == 1)
-    { // Чтение структуры Token из входного файла
-        int i;
-        for (i = 0; i < token.length; i++)
-        {                                                 // Цикл по длине повторяющегося фрагмента
-            char c = window[token.offset + i];            // Получение символа из скользящего окна
-            fwrite(&c, sizeof(char), 1, output);          // Запись символа в выходной файл
-            window[WINDOW_SIZE - 1] = c;                  // Добавление символа в конец скользящего окна
-            memmove(window, window + 1, WINDOW_SIZE - 1); // Сдвиг скользящего окна на один символ влево
+    {
+        if (isLiteral)
+        {
+            // Если предыдущий символ был литералом, восстанавливаем его
+            char c = token.next;
+            fwrite(&c, sizeof(char), 1, output);
+            window[WINDOW_SIZE - 1] = c;
+            memmove(window, window + 1, WINDOW_SIZE - 1);
+            isLiteral = 0;
         }
-
-        fwrite(&token.next, sizeof(char), 1, output); // Запись следующего символа в выходной файл
-        window[WINDOW_SIZE - 1] = token.next;         // Добавление следующего символа в конец скользящего окна
-        memmove(window, window + 1, WINDOW_SIZE - 1); // Сдвиг скользящего окна на один символ влево
+        else if (token.length == 0)
+        {
+            char c = token.next;
+            if (c == 0)
+            {
+                // Если текущий символ - литерал, устанавливаем флаг
+                isLiteral = 1;
+            }
+            else
+            {
+                fwrite(&c, sizeof(char), 1, output);
+                window[WINDOW_SIZE - 1] = c;
+                memmove(window, window + 1, WINDOW_SIZE - 1);
+            }
+        }
+        else
+        {
+            int i;
+            for (i = 0; i < token.length; i++)
+            {
+                char c = window[token.offset + i];
+                fwrite(&c, sizeof(char), 1, output);
+                window[WINDOW_SIZE - 1] = c;
+                memmove(window, window + 1, WINDOW_SIZE - 1);
+            }
+            if (token.next != 0)
+            {
+                fwrite(&token.next, sizeof(char), 1, output);
+            }
+            window[WINDOW_SIZE - 1] = token.next;
+            memmove(window, window + 1, WINDOW_SIZE - 1);
+        }
     }
 }
 
 int main(int argc, char *argv[])
-{ // Главная функция программы
+{
     if (argc < 5)
-    {                                                                    // Если количество аргументов меньше 5
-        printf("Usage: lz77compress -c/-d -o output_file input_file\n"); // Вывод сообщения об использовании программы
-        return 1;                                                        // Возврат кода ошибки
+    {
+        printf("Usage: lz77 -c/-d -o output_file input_file\n");
+        return 1;
     }
 
-    char *option = argv[1];         // Получение опции из аргументов командной строки
-    char *outputFileName = argv[3]; // Получение имени выходного файла из аргументов командной строки
-    char *inputFileName = argv[4];  // Получение имени входного файла из аргументов командной строки
+    char *option = argv[1];
+    char *outputFileName = argv[3];
+    char *inputFileName = argv[4];
 
-    FILE *inputFile = fopen(inputFileName, "r");   // Открытие входного файла для чтения
-    FILE *outputFile = fopen(outputFileName, "w"); // Открытие выходного файла для записи
+    FILE *inputFile = fopen(inputFileName, "rb");
+    FILE *outputFile = fopen(outputFileName, "wb");
 
     if (inputFile == NULL || outputFile == NULL)
-    {                                    // Если не удалось открыть входной или выходной файл
-        printf("Error opening files\n"); // Вывод сообщения об ошибке
-        return 1;                        // Возврат кода ошибки
+    {
+        printf("Error opening files\n");
+        return 1;
     }
 
     if (strcmp(option, "-c") == 0)
-    {                                    // Если опция -c
-        compress(inputFile, outputFile); // Вызов функции сжатия
+    {
+        compress(inputFile, outputFile);
     }
     else if (strcmp(option, "-d") == 0)
-    {                                      // Если опция -d
-        decompress(inputFile, outputFile); // Вызов функции распаковки
+    {
+        decompress(inputFile, outputFile);
     }
     else
-    {                               // Если опция некорректна
-        printf("Invalid option\n"); // Вывод сообщения об ошибке
-        return 1;                   // Возврат кода ошибки
+    {
+        printf("Invalid option\n");
+        return 1;
     }
 
-    fclose(inputFile);  // Закрытие входного файла
-    fclose(outputFile); // Закрытие выходного файла
+    fclose(inputFile);
+    fclose(outputFile);
 
-    return 0; // Возврат нулевого кода, если программа завершилась без ошибок
+    return 0;
 }
